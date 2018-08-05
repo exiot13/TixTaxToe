2cd15f05b0c51e7758e03a2017280f25e271cda5
exiot13 <ash_draco_1@hotmail.com>
2018-08-05 19:23:41 -0400

    Get set

---
+++ TixTaxToe/Source/Tile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TixTaxToe.Source
+{
+    class Tile
+    {
+        public Rectangle Border;
+        const int SIZE = 64;
+        public Team PTeam = Team.BLANK;
+        public Texture2D Image { get; set; }
+
+
+        public Tile()
+        {
+
+
+        }
+    }
+    public enum Team
+    {
+        X,O,BLANK
+
+    }
+
+
+
+}
